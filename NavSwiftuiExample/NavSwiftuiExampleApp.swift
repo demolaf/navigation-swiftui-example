28b2a8e70f884bf1f51744752c7a5cbb6f6f9890
Ademola Fadumo <demolafadumo@gmail.com>
2024-11-24 14:17:15 +0100

    Initial Commit

---
+++ NavSwiftuiExample/NavSwiftuiExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavSwiftuiExampleApp.swift
+//  NavSwiftuiExample
+//
+//  Created by Ademola Fadumo on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavSwiftuiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
